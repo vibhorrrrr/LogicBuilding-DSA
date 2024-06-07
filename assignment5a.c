2129006f88acc4b827728035396262af52777c61
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-06-08 00:02:12 +0530

    Add files via upload

---
+++ assignment5a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    for (iCnt = 0; iCnt<=iNo; iCnt++)
+    {
+        printf("$\t*\t");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+    
+    return 0;
+}
