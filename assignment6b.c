7b452042ea2e20ded97be847c05e6d99c7b3f9c9
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-06-09 01:09:58 +0530

    Add files via upload

---
+++ assignment6b.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo<0)
+    {
+        iNo = -(iNo);
+    }
+    if(iNo == 1)
+    {
+        printf("One");
+    }
+    else if(iNo == 2)
+    {
+        printf("Two");
+    }
+    else if(iNo == 3)
+    {
+        printf("Three");
+    }
+    else if(iNo == 4)
+    {
+        printf("Four");
+    }
+    else if(iNo == 5)
+    {
+        printf("Five");
+    }
+    else if(iNo == 6)
+    {
+        printf("Six");
+    }
+    else if(iNo == 7)
+    {
+        printf("Seven");
+    }
+    else if(iNo == 8)
+    {
+        printf("Eight");
+    }
+    else if(iNo == 9)
+    {
+        printf("Nine");
+    }
+    else
+    {
+        printf("Invalid Number");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+    
+    return 0;
+}
