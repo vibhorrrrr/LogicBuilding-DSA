ed35967a2e00f987b0e42e0c6ce09ee00dd39636
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-06-14 01:35:07 +0530

    Add files via upload

---
+++ assignment11a.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int CountEven(int iNo)
+{
+    int iDigit=0, iCnt= 0;
+    if(iNo<0)
+    {
+        iNo = -(iNo);
+    }
+    while(iNo>0)
+    {
+        iDigit = iNo % 10;
+        if((iDigit % 2) == 0)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    int iRet = 0;
+    iRet = CountEven(iValue);
+    
+    printf("%d", iRet);
+
+    return 0;
+}
