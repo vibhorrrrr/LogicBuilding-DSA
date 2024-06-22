eafe090913fbe8b3968fcbd610e7052d70fa6b20
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-06-23 00:45:19 +0530

    Add files via upload

---
+++ assignment18b.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i + j > iRow + 1)
+            {
+                printf("@\t");
+            }
+            else if (i + j > iRow)
+            {
+                printf("#\t");
+            }
+            else 
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: \n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
