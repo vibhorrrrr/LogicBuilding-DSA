0d113b34adbdfaf7e1a3a7057d8b1f9eb3618643
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-06-26 23:18:43 +0530

    Update doublyll.c

--- doublyll.c
+++ doublyll.c
@@ -103,4 +103,89 @@ void InsertAtPos(PPNODE First, int No, int iPos)
         newn->prev = temp;          // $
     }
 }
+void DeleteFirst(PPNODE First)
+{
+    if(*First == NULL)  // Case 1
+    {
+        printf("Unable to delete the node as the linked list is empty\n");
+        return;
+    }
+    else if((*First)->next == NULL) // Case 2
+    {
+        free(*First);
+        *First = NULL;
+    }
+    else    // Case 3
+    {
+        *First = (*First)->next;
+        free((*First)->prev);   // $
+        (*First)->prev = NULL;    // $
+    }
+}
+
+void DeleteLast(PPNODE First)
+{
+    PNODE temp = NULL;
+    if(*First == NULL)  // Case 1
+    {
+        printf("Unable to delete the node as the linked list is empty\n");
+        return;
+    }
+    else if((*First)->next == NULL) // Case 2
+    {
+        free(*First);
+        *First = NULL;
+    }
+    else    // Case 3
+    {
+        temp = *First;
+
+        while(temp->next->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL; 
+    }
+}
+
+void DeleteAtPos(PPNODE First, int iPos)
+{
+    
+    int iLength=0;
+    int i = 0;
+    PNODE temp = NULL;
+
+    iLength = Count(*First);
+
+    if((iPos<1)|| (iPos>iLength))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        DeleteFirst(First);
+    }
+    else if(iPos == iLength)
+    {
+        DeleteLast(First);
+    }
+    else
+    {
+        temp = *First;
+
+        for(i = 1; i < iPos-1; i++)
+        {
+            temp = temp->next;
+        }
+
+        temp->next=temp->next->next;
+        free(temp->next->prev);
+        temp->next->prev = temp;
+    }
+}
+
+
 
