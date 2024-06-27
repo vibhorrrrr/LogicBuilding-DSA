49cd3ce3ab38cfdb28567b391e9a006c721a38b9
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-06-27 23:14:58 +0530

    Update Singlyll.cpp

--- Singlyll.cpp
+++ Singlyll.cpp
@@ -9,3 +9,24 @@ struct node
 
 typedef struct node NODE;
 typedef struct node * PNODE;
+
+class SinglyLL
+{
+    private:
+        PNODE First;
+        int iCount;
+
+    public:
+        SinglyLL(); 
+
+        void Display();
+        int Count();
+
+        void InsertFirst(int No);
+        void InsertLast(int No);
+        void InsertAtPos(int No, int iPos);
+
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int iPos);
+};
