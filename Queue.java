bc6f7967a0a894810aa1b14d445c42c4097a35f6
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-07-02 23:41:35 +0530

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -22,3 +22,41 @@ class Queue //FIFO
         First = null;
         iCount = 0;
     }
+
+    public void EnQueue(int No)    //InsertLast
+    {  
+        node newn = new node(No); 
+        if(First == null)
+        {
+            First = newn;
+        }
+        else
+        {
+            node temp = First;
+
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+            }
+            temp.next=newn;
+        }
+        iCount++;
+    }
+
+    public int DeQueue()   //DeleteFirst
+    {
+        int Value = 0;
+
+        if(First == null)
+        {
+            System.out.println("\nQueue is Empty");
+            return -1;
+        }
+        else
+        {
+            Value = First.data;
+            First = First.next;
+        }
+        iCount--;
+        return Value;
+    }
