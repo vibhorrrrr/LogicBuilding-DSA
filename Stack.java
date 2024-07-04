1202852bb6eb7e5c1fd02167979013a0b211cdbc
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-07-04 14:51:41 +0530

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -8,4 +8,24 @@ class Node {
     }
 }
 
+class Stack {
+    public Node First;
+    public int iCount;
+
+    public Stack() {
+        System.out.println("\nObject of Stack gets created successfully");
+        First = null;
+        iCount = 0;
+    }
+
+    public void Push(int number) {
+        Node newNode = new Node(number);
+        if (First == null) {
+            First = newNode;
+        } else {
+            newNode.next = First;
+            First = newNode;
+        }
+        iCount++;
+    }
 
