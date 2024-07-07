42d7e414cb9909d5d7c4f7f039af15a19c6335ed
Vibhor Joshi <139653660+vibhorrrrr@users.noreply.github.com>
2024-07-07 10:31:40 +0530

    Create files.c

---
+++ files.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int fd = 0;
+    char FileName[30];
+    char Buffer[100]={'\0'};
+    int iRet = 0;
+    int i = 0, iCount = 0;
+    char ch = '\0'
+
+    printf("Enter the file name that you want to create: \n");
+    scanf("%s", FileName);
